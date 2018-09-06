ed79a25ab2fe644ae7fcaa1b93396eaaccfccaf3
Nam Wook Kim <namwkim85@gmail.com>
2018-09-06 10:48:51 -0400

    listen to public ip

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 
 if __name__ == '__main__':
 	# socketio.run(app, port=app.config['PORT'], host='0.0.0.0', debug=True)
-	app.run(port=app.config["PORT"], debug=app.config["DEBUG"])
+	app.run(host='0.0.0.0', port=app.config["PORT"], debug=app.config["DEBUG"])
